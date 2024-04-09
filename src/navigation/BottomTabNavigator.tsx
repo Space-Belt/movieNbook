97e3c44eda6a07ea95f91884020a52bc4f29b77a
OnlyWooo <dngurwn@gmail.com>
2024-04-09 18:17:55 +0900

    bottomTabNavigator 추가 및 스크린 생성

--- src/navigation/BottomTabNavigator.tsx
+++ src/navigation/BottomTabNavigator.tsx
@@ -2,9 +2,13 @@
 // import CustomIcon from '../components/icons/CustomIcon';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import React from 'react';
-import {StyleSheet} from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import CustomIcon from '../components/icons/CustomIcon';
 import HomeScreen from '../screens/HomeScreen';
-import {COLORS} from '../theme/theme';
+import SearchScreen from '../screens/SearchScreen';
+import TicketScreen from '../screens/TicketScreen';
+import UserScreen from '../screens/UserScreen';
+import {COLORS, FONTSIZE, SPACING} from '../theme/theme';
 
 const Tab = createBottomTabNavigator();
 
@@ -16,13 +20,108 @@ const BottomTabNavigator = () => {
         headerShown: false,
         tabBarStyle: {
           backgroundColor: COLORS.Black,
+          borderTopWidth: 0,
+          height: SPACING.space_10 * 10,
         },
       }}>
-      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarShowLabel: false,
+          tabBarIcon: ({focused, color, size}) => {
+            return (
+              <View
+                style={[
+                  styles.activeTabBackground,
+                  focused ? {backgroundColor: COLORS.Orange} : {},
+                ]}>
+                <CustomIcon
+                  name="video"
+                  color={COLORS.White}
+                  size={FONTSIZE.size_30}
+                />
+              </View>
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={SearchScreen}
+        options={{
+          tabBarShowLabel: false,
+          tabBarIcon: ({focused, color, size}) => {
+            return (
+              <View
+                style={[
+                  styles.activeTabBackground,
+                  focused ? {backgroundColor: COLORS.Orange} : {},
+                ]}>
+                <CustomIcon
+                  name="search"
+                  color={COLORS.White}
+                  size={FONTSIZE.size_30}
+                />
+              </View>
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Ticket"
+        component={TicketScreen}
+        options={{
+          tabBarShowLabel: false,
+          tabBarIcon: ({focused, color, size}) => {
+            return (
+              <View
+                style={[
+                  styles.activeTabBackground,
+                  focused ? {backgroundColor: COLORS.Orange} : {},
+                ]}>
+                <CustomIcon
+                  name="ticket"
+                  color={COLORS.White}
+                  size={FONTSIZE.size_30}
+                />
+              </View>
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="User"
+        component={UserScreen}
+        options={{
+          tabBarShowLabel: false,
+          tabBarIcon: ({focused, color, size}) => {
+            return (
+              <View
+                style={[
+                  styles.activeTabBackground,
+                  focused ? {backgroundColor: COLORS.Orange} : {},
+                ]}>
+                <CustomIcon
+                  name="user"
+                  color={COLORS.White}
+                  size={FONTSIZE.size_30}
+                />
+              </View>
+            );
+          },
+        }}
+      />
     </Tab.Navigator>
   );
 };
 
-const style = StyleSheet.create({});
+const styles = StyleSheet.create({
+  activeTabBackground: {
+    backgroundColor: COLORS.Black,
+    padding: SPACING.space_18,
+    borderRadius: SPACING.space_18 * 10,
+  },
+});
 
 export default BottomTabNavigator;
