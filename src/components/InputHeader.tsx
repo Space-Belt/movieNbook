da3b1cb5931220ea222d177e5f88156cca0c06c8
OnlyWooo <dngurwn@gmail.com>
2024-04-09 21:53:56 +0900

    홈스크린 데이터 없을때 View

---
+++ src/components/InputHeader.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import {
+  BORDERRADIUS,
+  COLORS,
+  FONTFAMILY,
+  FONTSIZE,
+  SPACING,
+} from '../theme/theme';
+import CustomIcon from './icons/CustomIcon';
+
+interface Props {
+  searchFunction: (args: string) => void;
+}
+
+const InputHeader = ({searchFunction}: Props) => {
+  const [searchText, setSearchText] = React.useState<string>('');
+
+  return (
+    <View style={styles.inputBox}>
+      <TextInput
+        style={styles.textInput}
+        value={searchText}
+        onChangeText={textInput => setSearchText(textInput)}
+        placeholder="Search your Movies..."
+        placeholderTextColor={COLORS.WhiteRGBA32}
+      />
+      <TouchableOpacity
+        style={styles.searchIcon}
+        onPress={() => props.searchFunction(searchText)}>
+        <CustomIcon
+          name="search"
+          color={COLORS.Orange}
+          size={FONTSIZE.size_20}
+        />
+      </TouchableOpacity>
+      <Text style={styles.searchedText}>{searchText}</Text>
+    </View>
+  );
+};
+
+export default InputHeader;
+
+const styles = StyleSheet.create({
+  inputBox: {
+    paddingVertical: SPACING.space_8,
+    paddingHorizontal: SPACING.space_24,
+    borderWidth: 2,
+    borderColor: COLORS.WhiteRGBA15,
+    borderRadius: BORDERRADIUS.radius_25,
+    flexDirection: 'row',
+  },
+  textInput: {
+    width: '90%',
+    fontFamily: FONTFAMILY.poppins_regular,
+    fontSize: FONTSIZE.size_14,
+    color: COLORS.White,
+  },
+  searchIcon: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: SPACING.space_10,
+  },
+  searchedText: {
+    color: COLORS.White,
+  },
+});
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,12 +1,67 @@
+import {useNavigation} from '@react-navigation/native';
 import * as React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {
+  ActivityIndicator,
+  Dimensions,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  View,
+} from 'react-native';
+import InputHeader from '../components/InputHeader';
 import {COLORS} from '../theme/theme';
 
+const {width, height} = Dimensions.get('window');
+
+const getNowPlayingMoviesList = async () => {
+  try {
+  } catch (error) {}
+};
+
 const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  const [nowPlayingMoviesList, setNowPlayingMoviesList] =
+    React.useState<any>(undefined);
+  const [popularMoviesList, setPopularMoviesList] =
+    React.useState<any>(undefined);
+  const [upcomingMoviesList, setUpcomingMoviesList] =
+    React.useState<any>(undefined);
+
+  const searchMoviesFunction = () => {
+    navigation.navigate('Search');
+  };
+
+  if (
+    nowPlayingMoviesList == undefined &&
+    nowPlayingMoviesList == null &&
+    popularMoviesList == undefined &&
+    popularMoviesList == null &&
+    upcomingMoviesList == undefined &&
+    upcomingMoviesList == null
+  ) {
+    return (
+      <ScrollView
+        style={styles.container}
+        bounces={false}
+        contentContainerStyle={styles.scrollViewContainer}>
+        <StatusBar hidden />
+        <View>
+          <InputHeader searchFunction={searchMoviesFunction} />
+        </View>
+        <View style={styles.loadingContainer}>
+          <ActivityIndicator size={'large'} color={COLORS.Orange} />
+        </View>
+      </ScrollView>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>HomeScreen</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <StatusBar hidden />
+
+      <View style={styles.loadingContainer}></View>
+    </ScrollView>
   );
 };
 
@@ -14,6 +69,16 @@ export default HomeScreen;
 
 const styles = StyleSheet.create({
   container: {
+    display: 'flex',
     backgroundColor: COLORS.Black,
   },
+  scrollViewContainer: {
+    flex: 1,
+  },
+  loadingContainer: {
+    flex: 1,
+    alignSelf: 'center',
+    justifyContent: 'center',
+  },
+  inputHeaderContainer: {},
 });
