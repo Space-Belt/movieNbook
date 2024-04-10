cc846c495b973d37e66397c6f7db4390ab635d7a
OnlyWooo <dngurwn@gmail.com>
2024-04-10 15:38:23 +0900

    useQuery 사용을 위한 QueryClientProvider 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import {NavigationContainer} from '@react-navigation/native';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import React from 'react';
 import {StyleSheet} from 'react-native';
 import {RecoilRoot} from 'recoil';
 import MainScreen from './screens/MainScreen';
 
+const queryClient = new QueryClient();
+
 function App(): React.JSX.Element {
   return (
-    <RecoilRoot>
-      <NavigationContainer>
-        <MainScreen />
-      </NavigationContainer>
-    </RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <NavigationContainer>
+          <MainScreen />
+        </NavigationContainer>
+      </RecoilRoot>
+    </QueryClientProvider>
   );
 }
 
