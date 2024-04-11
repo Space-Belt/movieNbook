a066852bda485aad4d701875fc470b5ea09c71ee
OnlyWooo <dngurwn@gmail.com>
2024-04-11 13:02:12 +0900

    query key 추가

--- src/api/apiMovie.ts
+++ src/api/apiMovie.ts
@@ -18,6 +18,23 @@ export interface IMovie {
   vote_count: number;
 }
 
+export const queryKey = [
+  'id',
+  'adult',
+  'backdrop_path',
+  'genre_ids',
+  'original_language',
+  'original_title',
+  'overview',
+  'popularity',
+  'poster_path',
+  'release_date',
+  'title',
+  'video',
+  'vote_average',
+  'vote_count',
+];
+
 export const searchMovies = async (keyword: string): Promise<IMovie[]> => {
   const endPoint = `search/movie?api_key=${API_KEY}&query=${keyword}`;
   const response = (await apiClient.get(endPoint)).data.result;
@@ -25,11 +42,23 @@ export const searchMovies = async (keyword: string): Promise<IMovie[]> => {
   return response;
 };
 
-export const getNowPlayingMovies = async (): Promise<IMovie> => {
+export const getMovies = async (category: string): Promise<IMovie[]> => {
+  const endPoint = `/movie/${category}?api_key=${API_KEY}`;
+
+  try {
+    const response = (await apiClient.get(endPoint)).data.results;
+    return response;
+  } catch (error) {
+    console.error('Error fetching movies:', error);
+    throw error; // Rethrow the error to handle it at a higher level if needed
+  }
+};
+
+export const getNowPlayingMovies = async (): Promise<IMovie[]> => {
   const endPoint = `/movie/now_playing?api_key=${API_KEY}`;
   const response = (await apiClient.get(endPoint)).data;
 
-  // console.log(response);
+  console.log(response);
   return response;
 };
 
