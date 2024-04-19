880d2cb5375f2c2092f7b2f4b934835ea9f4521e
OnlyWooo <dngurwn@gmail.com>
2024-04-19 18:24:04 +0900

    LinearGradient 적용

--- src/screens/MovieDetailScreen.tsx
+++ src/screens/MovieDetailScreen.tsx
@@ -1,10 +1,20 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {
+  ActivityIndicator,
+  ImageBackground,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 
 import React from 'react';
 import {RootStackParamList} from './MainScreen';
 import {NativeStackScreenProps} from 'react-native-screens/lib/typescript/native-stack/types';
 import {useQuery} from '@tanstack/react-query';
 import {movieDetails, searchMovies} from '../api/apiMovie';
+import {COLORS} from '../theme/theme';
+import {baseImagePath} from '../api/apicalls';
+import LinearGradient from 'react-native-linear-gradient';
 
 type MovieProps = NativeStackScreenProps<
   RootStackParamList,
@@ -26,13 +36,42 @@ const MovieDetailScreen = ({route, navigation}: MovieProps) => {
   React.useEffect(() => {
     console.log(movieDetail);
   }, [movieDetail]);
-  return (
+
+  if (isLoading) {
     <View>
-      <Text>{route.params.movieId}</Text>
-    </View>
+      <ActivityIndicator size={'large'} color={COLORS.White} />
+    </View>;
+  }
+
+  return (
+    <ScrollView style={styles.container}>
+      <ImageBackground
+        source={{
+          uri: baseImagePath('w780', movieDetail?.backdrop_path),
+        }}
+        style={styles.imageBG}>
+        <LinearGradient
+          colors={[COLORS.BlackRGB10, COLORS.Black]}
+          style={styles.linearGradient}>
+          <Text style={{color: 'white'}}>Sign in with Facebook</Text>
+        </LinearGradient>
+      </ImageBackground>
+    </ScrollView>
   );
 };
 
 export default MovieDetailScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.Black,
+  },
+  imageBG: {
+    width: '100%',
+    aspectRatio: 3072 / 1727,
+  },
+  linearGradient: {
+    height: '100%',
+  },
+});
