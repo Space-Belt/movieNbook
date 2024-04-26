4fbf9d79cb18e0a005312f22547e8a6d0acbe92e
OnlyWooo <dngurwn@gmail.com>
2024-04-26 16:08:15 +0900

    안드로이드는 모달, ios는 ActionSheetIOS 적용

--- src/screens/EditProfileScreen.tsx
+++ src/screens/EditProfileScreen.tsx
@@ -1,4 +1,6 @@
 import {
+  ActionSheetIOS,
+  Platform,
   SafeAreaView,
   StyleSheet,
   Text,
@@ -14,13 +16,27 @@ import {userInfoState} from '../recoil/User';
 import ImageSelectWayModal from '../components/Profile/ImageSelectWayModal';
 import ReusableModal from '../components/Modal/ReusableModal';
 import EditableInfo from '../components/Profile/EditableInfo';
+import {
+  Asset,
+  CameraOptions,
+  ImageLibraryOptions,
+  ImagePickerResponse,
+  launchCamera,
+  launchImageLibrary,
+} from 'react-native-image-picker';
+
+const imagePickerOption = {
+  mediaType: 'photo',
+  maxWidth: 360,
+  maxHeight: 360,
+  includeBase64: Platform.OS === 'android',
+};
 
 const EditProfileScreen = () => {
   const myInfo = useRecoilValue(userInfoState);
 
-  const [profileImage, setProfileImage] = React.useState<string | null>(
-    myInfo.profileImage ? myInfo.profileImage : '',
-  );
+  const [profileImage, setProfileImage] = React.useState<Asset | null>(null);
+
   const [userName, setUserName] = React.useState<string>(
     myInfo.user_name ? myInfo.user_name : '',
   );
@@ -30,21 +46,66 @@ const EditProfileScreen = () => {
     setModalOpen(prev => !prev);
   };
 
+  const onPickImage = (res: any) => {
+    if (res.didCancel || !res) {
+      return;
+    }
+    console.log('PickImage', res);
+    setProfileImage(res.assets[0]);
+  };
+
+  const onLaunchCamera = () => {
+    launchCamera(imagePickerOption as CameraOptions, onPickImage);
+  };
+
+  const onLaunchImageLibrary = () => {
+    launchImageLibrary(imagePickerOption as ImageLibraryOptions, onPickImage);
+  };
+
+  const handleModalOpen = () => {
+    if (Platform.OS === 'android') {
+      setModalOpen(prev => !prev);
+    } else {
+      console.log('dfdfdfdfdfdfd');
+      ActionSheetIOS.showActionSheetWithOptions(
+        {
+          options: ['사진 찍기', '사진 선택', '취소'],
+          cancelButtonIndex: 2,
+        },
+        buttonIndex => {
+          if (buttonIndex === 0) {
+            onLaunchCamera();
+          } else if (buttonIndex === 1) {
+            onLaunchImageLibrary();
+          }
+        },
+      );
+    }
+  };
+
   return (
     <>
       <SafeAreaView style={styles.container}>
         <ProfileHeader title={'Edit Profile'} closeBtnVisible={true} />
         <EditableInfo
+          setProfileImage={setProfileImage}
           myInfo={myInfo}
           profileImage={profileImage}
           userName={userName}
           setModalOpen={setModalOpen}
+          handleModalOpen={handleModalOpen}
         />
       </SafeAreaView>
       <ReusableModal
         visible={modalOpen}
         onClose={modalClose}
-        children={<ImageSelectWayModal onClose={modalClose} />}
+        children={
+          <ImageSelectWayModal
+            onClose={modalClose}
+            onLaunchCamera={onLaunchCamera}
+            onLaunchImageLibrary={onLaunchImageLibrary}
+          />
+        }
       />
     </>
   );
