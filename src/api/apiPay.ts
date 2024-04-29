2c93423d85c2515de3a778c68b59bc42b17a2a7f
OnlyWooo <dngurwn@gmail.com>
2024-04-29 21:29:07 +0900

    payMethod, payHistory api 호출 추가

---
+++ src/api/apiPay.ts
@@ -0,0 +1,38 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {customApiClient} from './apiClient';
+
+export const getPayMethod = async () => {
+  const endPoint = 'payment/methods';
+  const token = await AsyncStorage.getItem('accessToken');
+
+  try {
+    const payWay = (
+      await customApiClient.get(endPoint, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+    ).data;
+    return payWay;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getPayHistory = async () => {
+  const endPoint = 'user/me/orders';
+  const token = await AsyncStorage.getItem('accessToken');
+  try {
+    const payWay = await customApiClient.get(endPoint, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    console.log('실행딤?');
+    console.log(payWay);
+    return payWay;
+  } catch (error) {
+    console.log(error);
+  }
+};
