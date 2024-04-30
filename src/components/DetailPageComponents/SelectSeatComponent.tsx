7c4138dbf2f0c6fa870025e8a46e016cb358dbf7
OnlyWooo <dngurwn@gmail.com>
2024-04-30 15:41:30 +0900

    날짜, 시간 선택

--- src/components/DetailPageComponents/SelectSeatComponent.tsx
+++ src/components/DetailPageComponents/SelectSeatComponent.tsx
@@ -42,6 +42,8 @@ const SelectSeatComponent = ({
   >([]);
   const [selectedDateIndex, setSelectedDateIndex] = React.useState<number>();
 
+  const [selectedTimeIndex, setSelectedTimeIndex] = React.useState<number>();
+
   const {data: movieDate, refetch: dateRefetch} = useQuery({
     queryKey: ['movieDate'],
     queryFn: () => getMovieDate(movieId),
@@ -50,6 +52,24 @@ const SelectSeatComponent = ({
     queryKey: ['movieSeats'],
     queryFn: () => getSeats(movieId),
   });
+
+  const renderItem = (item: any) => {
+    console.log(item);
+    return (
+      <TouchableOpacity
+        style={[
+          styles.timeWrapper,
+          item.index === selectedTimeIndex && {backgroundColor: COLORS.Orange},
+        ]}
+        onPress={() => {
+          setSelectedTimeIndex(item.index);
+        }}>
+        <Text style={{color: COLORS.White}}>
+          {item.item.start_time.slice(0, 5)}
+        </Text>
+      </TouchableOpacity>
+    );
+  };
 
   React.useEffect(() => {
     if (movieDate !== undefined) {
@@ -74,69 +94,86 @@ const SelectSeatComponent = ({
 
   return (
     <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
-      <View style={styles.topWrapper}>
-        <LinearHeader imagePath={poster} action={handleGoBack} />
-        <FlatList
-          data={availableDate}
-          keyExtractor={item => item.date}
-          horizontal
-          bounces={false}
-          contentContainerStyle={styles.containerGap24}
-          renderItem={({item, index}) => {
-            return (
-              <TouchableOpacity
-                onPress={() => {
-                  setSelectedDateIndex(index);
-                }}>
-                <View
-                  style={[
-                    styles.dateContainer,
-                    index === selectedDateIndex && {
-                      backgroundColor: COLORS.Orange,
-                    },
-                  ]}>
-                  <Text style={styles.dateText}>{item.date.split('-')[2]}</Text>
-                  <Text style={styles.dayText}>
-                    {item.dayOfWeek.slice(0, 3)}
-                  </Text>
-                </View>
-              </TouchableOpacity>
-            );
-          }}
-        />
-        <View style={styles.seatInfoContainer}>
-          <View style={styles.seatInfoWrapper}>
-            <CustomIcon name="radio" style={styles.seatInfoIcon} />
-            <Text style={styles.seatInfoText}>Available</Text>
-          </View>
-          <View style={styles.seatInfoWrapper}>
-            <CustomIcon
-              name="radio"
-              style={[styles.seatInfoIcon, {color: COLORS.Grey}]}
-            />
-            <Text style={styles.seatInfoText}>Taken</Text>
-          </View>
-          <View style={styles.seatInfoWrapper}>
-            <CustomIcon
-              name="radio"
-              style={[styles.seatInfoIcon, {color: COLORS.Orange}]}
-            />
-            <Text style={styles.seatInfoText}>Selected</Text>
-          </View>
+      {/* <View style={styles.topWrapper}> */}
+      <LinearHeader imagePath={poster} action={handleGoBack} />
+
+      <View style={styles.seatInfoContainer}>
+        <View style={styles.seatInfoWrapper}>
+          <CustomIcon name="radio" style={styles.seatInfoIcon} />
+          <Text style={styles.seatInfoText}>Available</Text>
         </View>
-        <View style={styles.buttonPriceContainer}>
-          <View style={styles.priceContainer}>
-            <Text style={styles.totalPriceText}>Total Price</Text>
-            <Text style={styles.price}>$ {movieDate?.price}.00</Text>
-          </View>
-          <TouchableOpacity
-            style={styles.buttonWrapper}
-            onPress={() => {
-              setPage(prev => prev + 1);
-            }}>
-            <Text style={styles.buttonText}>Buy Tickets</Text>
-          </TouchableOpacity>
+        <View style={styles.seatInfoWrapper}>
+          <CustomIcon
+            name="radio"
+            style={[styles.seatInfoIcon, {color: COLORS.Grey}]}
+          />
+          <Text style={styles.seatInfoText}>Taken</Text>
         </View>
+        <View style={styles.seatInfoWrapper}>
+          <CustomIcon
+            name="radio"
+            style={[styles.seatInfoIcon, {color: COLORS.Orange}]}
+          />
+          <Text style={styles.seatInfoText}>Selected</Text>
+        </View>
+      </View>
+      {/* </View> */}
+      <FlatList
+        data={availableDate}
+        keyExtractor={item => item.date}
+        horizontal
+        bounces={false}
+        contentContainerStyle={styles.containerGap24}
+        renderItem={({item, index}) => {
+          return (
+            <TouchableOpacity
+              onPress={() => {
+                setSelectedDateIndex(index);
+                setSelectedTimeIndex(undefined);
+              }}>
+              <View
+                style={[
+                  styles.dateContainer,
+                  index === selectedDateIndex && {
+                    backgroundColor: COLORS.Orange,
+                  },
+                ]}>
+                <Text style={styles.dateText}>{item.date.split('-')[2]}</Text>
+                <Text style={styles.dayText}>{item.dayOfWeek.slice(0, 3)}</Text>
+              </View>
+            </TouchableOpacity>
+          );
+        }}
+      />
+      <View style={styles.dateWrapper}>
+        {selectedDateIndex !== undefined &&
+        movieDate.result[availableDate[selectedDateIndex].date].length > 0 ? (
+          <FlatList
+            data={movieDate.result[availableDate[selectedDateIndex].date]}
+            renderItem={item => renderItem(item)}
+            horizontal
+            contentContainerStyle={styles.timeContainer}
+          />
+        ) : (
+          <View style={styles.noTicketBox}>
+            <Text style={styles.emptyText}>
+              Sorry, no tickets available at this date
+            </Text>
+          </View>
+        )}
+      </View>
+      <View style={styles.buttonPriceContainer}>
+        <View style={styles.priceContainer}>
+          <Text style={styles.totalPriceText}>Total Price</Text>
+          <Text style={styles.price}>$ {movieDate?.price}.00</Text>
+        </View>
+        <TouchableOpacity
+          style={styles.buttonWrapper}
+          onPress={() => {
+            setPage(prev => prev + 1);
+          }}>
+          <Text style={styles.buttonText}>Buy Tickets</Text>
+        </TouchableOpacity>
       </View>
     </ScrollView>
   );
@@ -207,6 +244,7 @@ const styles = StyleSheet.create({
   },
   seatInfoContainer: {
     flexDirection: 'row',
+
     marginTop: SPACING.space_36,
     marginBottom: SPACING.space_10,
     alignItems: 'center',
@@ -225,5 +263,31 @@ const styles = StyleSheet.create({
     fontFamily: FONTFAMILY.poppins_medium,
     fontSize: FONTSIZE.size_12,
     color: COLORS.White,
+  },
+  timeWrapper: {
+    height: 37,
+    width: 78,
+    borderWidth: 1,
+    borderColor: '#FFFFFF80',
+    borderRadius: 32,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  timeContainer: {
+    gap: 10,
+  },
+  noTicketBox: {
+    height: 37,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  dateWrapper: {
+    marginVertical: 15,
+  },
+  emptyText: {
+    color: COLORS.White,
+    fontSize: FONTSIZE.size_14,
+
+    textAlign: 'center',
   },
 });
