2d4227a9012397f94f27a78685f5360a6f4c79a7
OnlyWooo <dngurwn@gmail.com>
2024-05-07 18:02:29 +0900

    안드로이드 뒤로가기 처리

--- src/navigation/MainStackNavigator.tsx
+++ src/navigation/MainStackNavigator.tsx
@@ -1,15 +1,43 @@
 import {createStackNavigator} from '@react-navigation/stack';
 import React from 'react';
-import {StyleSheet} from 'react-native';
+
+import {Alert, BackHandler, StyleSheet} from 'react-native';
 import SignInScreen from '../screens/SignInScreen';
 import SignUpScreen from '../screens/SignUpScreen';
 import BottomTabNavigator from './BottomTabNavigator';
 import MovieDetailScreen from '../screens/MovieDetailScreen';
 import EditProfileScreen from '../screens/EditProfileScreen';
+import {RootStackParamList} from '../screens/MainScreen';
+import {navigationRef} from '../lib/navigation';
 
-const MainStack = createStackNavigator();
+const MainStack = createStackNavigator<RootStackParamList>();
 
 const MainStackNavigator = () => {
+  React.useEffect(() => {
+    const handleBackPress = () => {
+      if (navigationRef.getCurrentRoute()?.name === 'Home') {
+        Alert.alert('잠깐!!', '정말 앱을 종료하시겠어요?', [
+          {
+            text: '취소',
+            onPress: () => null,
+            style: 'cancel',
+          },
+          {text: '나가기', onPress: () => BackHandler.exitApp()},
+        ]);
+      } else {
+        navigationRef.goBack();
+      }
+
+      return true;
+    };
+
+    BackHandler.addEventListener('hardwareBackPress', () => handleBackPress());
+
+    return BackHandler.removeEventListener('hardwareBackPress', () =>
+      handleBackPress(),
+    );
+  }, []);
+
   return (
     <MainStack.Navigator
       screenOptions={{headerShown: false}}
